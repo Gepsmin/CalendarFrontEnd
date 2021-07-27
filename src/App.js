617876bc711d20b2bc8e1799094fd8e569f089f5
OnurSefa <onursefa_ozcibik@hotmail.com>
2021-07-27 23:43:35 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './css/base.css';
+import Home from './Home';
+
+function App() {
+  return (
+    <Home className="Home">
+    </Home>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Home extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            
+        }
+
+       
+    }
+
+
+    render() {
+        return <div>
+            Hello World!
+        </div>
+            
+    }
+}
+
+
+export default Home
